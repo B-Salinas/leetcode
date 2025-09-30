88b605921fda601fab7f0f7a0ec7b45aa09fd400
B <b.salinas397@gmail.com>
2025-09-30 03:31:30 -0500

    Update 214-shortest-palindrome-1.js
    going to delete this file

--- problems/hard/214-shortest-palindrome/214-shortest-palindrome-1.js
+++ problems/hard/214-shortest-palindrome/214-shortest-palindrome-1.js
@@ -1,16 +1,1 @@
-/**
-You are given a string `s`. You can convert `s` to a palindrome* by adding characters in front of it.
 
-Return *the shortest palindrome you can find by performing this transformation*.
-
-* A **palindrome** is a string that reads the same forward and backward.
- */
-
-/**
- * @param {string} s
- * @return {string}
- */
-
-var shortestPalindrome = function(s) {
-    
-};
